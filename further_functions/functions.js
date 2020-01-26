9ad2998b0038b6a8f77528763c579a93f9be2742
pedroRelvas <43316469+pedroRelvas@users.noreply.github.com>
2020-01-26 18:09:26 +0000

    More functions
    -IIFE
    -Functions that define and rewrite themselves
    -Init-Time Branching
    -Factorial Number

--- further_functions/functions.js
+++ further_functions/functions.js
@@ -4,7 +4,6 @@ function sayHello(gretting = "Hello") {
 
 //watch my homie call() takin' up this function fo' good
 
-const clark = { name: "Clark" };
 const zezinho = { name: "Zézinho Cigano" };
 
 document.write(sayHello.call(zezinho, "Faaaai"));
@@ -21,5 +20,76 @@ function square(x) {
 
 square(3);
 square(-11);
+square(4);
 
 console.log(square.cache);
+
+//Immediately invoked Function Expressions
+
+(function() {
+  const temp = "World";
+  console.log(`Hello ${temp}`);
+})();
+
+(function() {
+  const dragao = "desunião";
+  console.log(`Ai estes são os filhos da ${dragao}`);
+})();
+//work the same way
+{
+  const name = "Peter Parker";
+  const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ];
+  const date = new Date(),
+    today = days[date.getDay()];
+  console.log(`Welcome back ${name}, Today is ${today}!`);
+}
+
+//-Functions that define and rewrite themselves
+function party() {
+  console.log("Wow this is amazing");
+  party = function() {
+    console.log("Estou triste.");
+  };
+}
+
+party();
+party();
+
+//Init-time Branching
+
+function ride() {
+  if (window.unicorn) {
+    ride = function() {
+      return "Riding on a unicorn is the best!";
+    };
+  } else {
+    ride = function() {
+      return "Riding on a pony is still pretty good";
+    };
+  }
+  return ride();
+}
+
+console.log(ride());
+
+//Facturial Number
+
+function factorial(n) {
+  if (n === 0) {
+    return 1;
+  } else {
+    //don't get it, yet
+    return n * factorial(n - 1);
+  }
+}
+
+console.log(factorial(0));
+console.log(factorial(4));
