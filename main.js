18add3720b33db59b1a495ae31c3d1b411aedbed
pedroRelvas <43316469+pedroRelvas@users.noreply.github.com>
2020-03-07 15:58:37 +0000

    First question Interview
    th first one

--- main.js
+++ main.js
@@ -1,204 +1,36 @@
-//const form = document.forms[0] OR
-//const form = document.getElementsByTagName('form')[0]; OR
-//const form = document.forms.search; //"search" is the name attribute OR
-const form = document.forms["search"];
-const formRaiz = document.forms["formRaiz"];
-//form object
-const [input, button] = form.elements;
-const inputLouco = form["searchInput"];
-input.value = "Aqui pa";
-//inputLouco.addEventListener("focus", () => alert("call me"), false);
-//inputLouco.addEventListener("blur", () => alert("don't call me"), false);
-inputLouco.addEventListener("change", () => alert("hey buddy"), false);
-form.addEventListener("submit", search, false);
+//** 1 */
 
-function search(event) {
-  alert(`You searched for: ${inputLouco.value}`);
-  event.preventDefault();
-}
+//The pitfall is in that null is also considered an object, as well arrays.
+//Soon if we type the follwing code, it will return true!
+const bar = [];
+console.log(typeof bar === "object");
 
-inputLouco.addEventListener(
-  "focus",
-  () => {
-    if (inputLouco.value === "Aqui pa") {
-      inputLouco.value = "";
-      throw new Error("motherfucker");
-    }
-  },
-  false
+//As long as one is aware of this, the problem can easily be avoided by also checking if bar is null:
+console.log(bar != null && typeof bar === "object");
+
+//the above solution wil return false if bar is a function. this is what we desire, but if we want to return true for functions , we can to the folwing code:
+console.log(
+	(bar != null && typeof bar === "object") || typeof bar === "function"
 );
 
-inputLouco.addEventListener(
-  "blur",
-  () => {
-    if (inputLouco.value === "") {
-      inputLouco.value = "Aqui pa";
-    }
-  },
-  false
+//the above solution will return true if bar is an array, because arrays are indeed objects
+//so this a solution to return false if bar is an array
+//see this to better explanation:https://luxiyalu.com/object-prototype-tostring-call/
+console.log(
+	bar != null &&
+		typeof bar === "object" &&
+		toString.call(bar) !== "[object Array]"
 );
 
-//form.action = "www.facebook.com";
-console.log(form);
-console.log(button);
-console.log(inputLouco);
+const hey = "heyhye!";
+console.log(toString.call(hey) === "[object String]");
 
-function squareRoot(number) {
-  if (number < 0) {
-    throw new RangeError("You can't handle the truth");
-  }
-  return Math.sqrt(number);
-}
+//However, there's one other alternative that returns false for nulls, arrays, and functions, but true for objects:
+console.log(bar !== null && bar.constructor === Object);
+console.log(bar.constructor);
 
-console.log(squareRoot(121));
-
-function imaginarySquareRoot(number) {
-  let answer;
-  try {
-    answer = String(squareRoot(number));
-  } catch (error) {
-    answer = squareRoot(-number) + "i";
-  } finally {
-    return `+ or - ${answer}`;
-  }
-}
-
-console.log(imaginarySquareRoot(-49));
-
-/* const raizQuadradaInput = document.getElementsByName(formRaiz["raizQuadrada"]);
-const raizQuadradaInputValue = raizQuadradaInput.value;
-const getRaizQuadradaBtn = document.getElementById("getSquareRoot");
-
-
-function displayRaizQuadrada(){
-
-  getRaizQuadradaBtn.addEventListener(
-    "click",
-    function squareRoot(number) {
-      if (number < 0) {
-        throw new RangeError("You can't handle the truth");
-      }
-      return Math.sqrt(number);
-    },
-    false
-  );
-  
-
-}
- */
-
-function collatz(n, sequence = [n]) {
-  if (n === 1) {
-    return `Sequence took ${sequence.length} steps. It was ${sequence}`;
-  }
-
-  if (n % 2 === 0) {
-    n = n / 2;
-  } else {
-    n = 3 * n + 1;
-  }
-
-  return collatz(n, [...sequence, n]);
-}
-
-console.log(collatz(29));
-
-function cattz(numero, sequencia = [numero]) {
-  if (numero === 1) {
-    return `cabou-se. passos ${sequencia.length}. numeros: ${sequencia} `;
-  }
-
-  if (numero % 2 === 0) {
-    numero = numero / 2;
-  } else {
-    numero = 3 * numero + 1;
-  }
-
-  return cattz(numero, [...sequencia, numero]);
-}
-
-console.log(cattz(90));
-
-//creating a promise
-//object
-const dice = {
-  sides: 6,
-  roll() {
-    return Math.floor(this.sides * Math.random()) + 1;
-  }
-};
-
-/* console.log("Before the roll");
-
-const roll = new Promise((resolve, reject) => {
-  const n = dice.roll();
-  if (n > 1) {
-    setTimeout(() => {
-      resolve(n);
-    }, n * 200);
-  } else {
-    setTimeout(() => reject(n), n * 200);
-  }
-});
-
-roll
-  .then(result => console.log(`I rolled a ${result}`))
-  .catch(result => console.log(`Drat! ... I rolled a ${result}`));
-console.log("After the roll!");
-
- */
-//Objeto
-const carro = {
-  portas: 4,
-  abertas() {
-    return Math.floor(Math.random() * (this.portas + 1));
-  }
-};
-
-const abertas = new Promise((resolve, reject) => {
-  const aberta = carro.abertas();
-  if (aberta >= 1) {
-    setTimeout(() => {
-      resolve(aberta);
-    }, 200);
-  } else if (aberta === 4) {
-    setTimeout(() => {
-      resolve(aberta);
-    }, aberta * 100);
-  } else {
-    setTimeout(() => {
-      reject(aberta);
-    }, 200);
-  }
-});
-
-abertas
-  .then(result => console.log(`Estão cerca de ${result} portas abertas.`))
-  .then(result => console.log(`Todas as ${result} portas estão abertas.`))
-  .catch(result => console.log(`Estão ${result} portas abertas!`));
-
-function random(a, b = 1) {
-  if (b === 1) {
-    [a, b] = [b, a];
-  }
-
-  return console.log(Math.floor((b - a + 1) * Math.random() + a));
-}
-
-random(1, 6);
-
-//more flexibity
-function randomSecond(a, b, callback) {
-  if (b === undefined) (b = a), (a = 1);
-  const result = Math.floor((b - a + 1) * Math.random()) + a;
-  if (callback) {
-    result = callback(result);
-  }
-  return result;
-}
-
-function square(n) {
-  return n * n;
-}
-
-console.log("hey" + randomSecond(1, 6, square));
+//The constructor property returns a reference to the
+//Object constructor function that created the instance
+//object. Note that the value of this property is a
+//reference to the function itself, not a string
+//containing the function's name
